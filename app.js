247c5b82f2a28c0ef281430df3e80b77aed00469
Dominik Brame <idominikbrame@gmail.com>
2021-02-10 19:32:32 -0600

    added landing animation, portflio section

--- app.js
+++ app.js
@@ -1,16 +1,100 @@
-window.addEventListener('load', function(e) {
-  setTimeout(videoOpacity, 700)
-  setTimeout(showNav, 1000)
+//gsap
+gsap.registerPlugin(ScrollTrigger)
+
+gsap.from('.header', {duration: 1, opacity: 0, ease: 'bounce'})
+gsap.from('.nav-item', {duration: 1, opacity:0, y: '-100%', stagger: .2, ease:'bounce'})
+gsap.from('.side-nav li', {duration: 1, opacity:0, y: '-100%', ease:'bounce', stagger: .1})
+gsap.from('.video-container #landing-video', {duration: 1.5, opacity: 0, transform:"scale(1.05)"})
+
+//register scroll plugin
+
+gsap.from(".box", {
+  x: -100,
+  duration: 1,
+  rotation:15,
+  scrollTrigger: {
+    trigger: ".box",
+    start: "top 60%",
+    markers: true,
+  }
 })
 
-const showNav = () => {
-  document.querySelector('.aside').style.opacity = "1";
-  document.querySelector('header').style.opacity = "1";
-}
+gsap.from(".box2", {
+  x: -100,
+  duration: 1,
+  rotation:15,
+  scrollTrigger: {
+    trigger: ".box2",
+    start: "top 60%",
+    markers: true,
+  }
+})
+
+gsap.from(".box3", {
+  x: -100,
+  duration: 1,
+  rotation:15,
+  scrollTrigger: {
+    trigger: ".box3",
+    start: "top 60%",
+    markers: true,
+  }
+})
+
+gsap.from(".box4", {
+  x: -100,
+  duration: 1.5,
+  rotation:25,
+  scrollTrigger: {
+    trigger: ".box4",
+    start: "top 60%",
+    markers: true,
+  }
+})
 
 
-const videoOpacity = () => {
-  let landingVideo = document.querySelector('#landing-video').style
-  landingVideo.opacity = "1";
-  landingVideo.transform = "scale(.95)"
-}
+
+
+document.querySelector(".gallery1").addEventListener("mouseover", e  => {
+      gsap.to(".gallery1", {
+        x:-900,
+        duration: 1.5,
+      })
+      gsap.to(".gallery1-item", {
+        delay: 1,
+        x: 50,
+        duration: 2,
+        opacity: 1
+      })
+  })
+
+
+  
+document.querySelector(".gallery2").addEventListener("mouseover", e  => {
+  gsap.to(".gallery2", {
+    x:-900,
+    duration: 1.5,
+  })
+  gsap.to(".gallery2-item", {
+    delay: 1,
+    x: 50,
+    duration: 2,
+    opacity: 1
+  })
+})
+
+
+
+
+document.querySelector(".gallery3").addEventListener("mouseover", e  => {
+  gsap.to(".gallery3", {
+    x:-900,
+    duration: 1.5,
+  })
+  gsap.to(".gallery3-item", {
+    delay: 1,
+    x: 50,
+    duration: 2,
+    opacity: 1
+  })
+})
